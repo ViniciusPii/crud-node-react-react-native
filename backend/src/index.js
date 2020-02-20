17362ed5ef5fbd11040cfa72bc287f382ad20dc3
Pii <pii@Piis-iMac.local>
2020-02-19 22:11:30 -0300

    Criação do routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  return res.send('Dev, seja bem vindo');
-});
+app.use(cors());
+app.use(require('./routes'));
+
 
 app.listen(3000);

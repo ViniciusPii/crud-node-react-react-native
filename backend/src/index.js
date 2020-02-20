29f2d5cc3620ad0752be32694539626a0da29109
Pii <pii@Piis-iMac.local>
2020-02-19 22:21:01 -0300

    Conexão com o banco de dados

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv/config');
 
 const app = express();
+
+mongoose.connect(process.env.MONGOOSE_CONNECT, {
+  useNewUrlParser: true,
+  useUnifiedTopology:true
+})
 
 app.use(cors());
 app.use(require('./routes'));

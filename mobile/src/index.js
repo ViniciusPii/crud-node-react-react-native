75f61acee2275dc17f0048878d8cb2943523715e
Vinicius Sales <viniciussales@MacBook-Pro-de-Vinicius.local>
2020-02-24 16:00:59 -0300

    Criação do list

---
+++ mobile/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+import ListUsers from './pages/list-users/ListUsers';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <ListUsers></ListUsers>
+    </SafeAreaView>
+  );
+};
+
+export default App;
---
+++ mobile/src/pages/list-users/ListUsers.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+const ListUsers = () => {
+  return (
+    <SafeAreaView>
+      <Text>Olá ListUsers</Text>
+    </SafeAreaView>
+  );
+};
+
+export default ListUsers;
